763107a236349c74680eda3908a6ee943f2a5d3c
Yohannes G.Simegne <ygetahun37@gmail.com>
2024-08-12 18:05:51 +0300

    Sections added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Pages/Home/Navbar";
-import Home from "./Pages/Home/Homescreen";
+import Home from "./Pages/Home/Homescreen/index.js";
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
           <Navbar />
           <Routes>
             <Route path="/" element={<Home />}></Route>
-            <Route path="*" element={<div> 404 Not Found </div>}></Route>
+            <Route path="*" element={<div>404 Not Found</div>}></Route>
           </Routes>
         </div>
       </Router>
