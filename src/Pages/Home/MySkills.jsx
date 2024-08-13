7390387a785db236de2e7312a70a39ad95473702
Yohannes G.Simegne <ygetahun37@gmail.com>
2024-08-13 13:41:35 +0300

    Ui update

--- src/Pages/Home/MySkills.jsx
+++ src/Pages/Home/MySkills.jsx
@@ -5,13 +5,13 @@ export default function MySkills() {
     <section className="skills--section" id="mySkills">
       <div className="portfolio--container">
         <p className="section--title">My Skills</p>
-        <h2 className="skills--section--heading">My Expertise</h2>
+        <h2 className="skills--section--heading">My Skills</h2>
       </div>
       <div className="skills--section--container">
         {data?.skills?.map((item, index) => (
           <div key={index} className="skills--section--card">
             <div className="skills--section--img">
-              <img src={item.src} alt="Product Chain" />
+              <img src={item.src} alt="logos" />
             </div>
             <div className="skills--section--card--content">
               <h3 className="skills--section--title">{item.title}</h3>
